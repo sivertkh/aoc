d94af45d30c8d03a4502a10f45bd3f0a559909d1
Sivert Hatteberg <sivert.hatteberg@usit.uio.no>
2017-12-12 13:04:50 +0100

    Pycharm stuff and the first day ish

---
+++ 2017/day-1/day1-2.py
@@ -0,0 +1,19 @@
+
+with open('./input.txt', 'r') as fp:
+    data = map(int, fp.read().rstrip())
+
+skip = len(data)/2
+l = len(data)
+
+# k = data[(i+skip) % len(data)]
+
+
+s = 0
+
+for i, v in enumerate(data):
+
+    if v == data[(i+skip) % l]:
+        s = s + v
+
+print s
+
