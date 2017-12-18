cdd32e6a54aba45a56847dcad910d3ce97f79f87
Sivert Hatteberg <sivert.hatteberg@usit.uio.no>
2017-12-18 17:45:01 +0100

    day-8 ok

---
+++ 2017/day-8/day-8-2.py
@@ -0,0 +1,44 @@
+# --- Day 8: I Heard You Like Registers ---
+# part 2 - ok
+
+register = {}
+
+
+def get_register_value(name):
+
+    if name not in register:
+        register[name] = 0
+
+    return register[name]
+
+
+with open('input.txt', 'r') as fp:
+    inst = [x.rstrip().split(" ") for x in fp.read().split('\n')]
+
+t = [x[1] for x in inst]
+
+highest = 0
+
+for i in inst:
+    reg = i[0]
+    instruction = i[1]
+    value = int(i[2])
+
+    if_reg = get_register_value(i[4])
+
+    if eval("{}{}{}".format(if_reg, i[5], i[6])):
+        if instruction == 'inc':
+            if reg in register:
+                register[reg] = register[reg] + value
+            else:
+                register[reg] = 0 + value
+        elif instruction == 'dec':
+            if reg in register:
+                register[reg] = register[reg] - value
+            else:
+                register[reg] = 0 - value
+
+        if register[reg] > highest:
+            highest = register[reg]
+
+print(highest)
