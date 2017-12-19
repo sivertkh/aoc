f6d983f536c56fa3c6b452d5cf2b52e942b1d2d9
Sivert Hatteberg <sivertkh@ifi.uio.no>
2017-12-19 22:57:54 +0100

    part 1 ok

---
+++ 2017/day-17/day-17-1.py
@@ -0,0 +1,27 @@
+# --- Day 17: Spinlock ---
+# part 1 - ok
+
+from collections import deque
+
+buffer = deque()
+buffer.append(0)
+
+cur_pos = 0
+step = 316
+
+for i in range(1,2018):
+    # step step nr of places
+    # if it wrap -> move to
+    new_pos = cur_pos
+
+    for a in range(step):
+        new_pos = new_pos + 1
+
+        if new_pos >= len(buffer):
+            new_pos = 0
+    # insert i after the current position.
+    buffer.insert(new_pos+1, i)
+    cur_pos = new_pos+1
+
+print(list(buffer)[cur_pos-4:cur_pos+4])
+print(buffer[cur_pos+1])
