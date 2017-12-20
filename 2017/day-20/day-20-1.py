81393122b697b24f9f33f45bb1f5c9a80cff71b0
Sivert Hatteberg <sivert.hatteberg@usit.uio.no>
2017-12-20 09:19:41 +0100

    part 1 ok

--- 2017/day-20/day-20-1.py
+++ 2017/day-20/day-20-1.py
@@ -1,58 +1,28 @@
 # --- Day 20: Particle Swarm ---
-# part 1 -
-
+# part 1 - ok
 
 particles = {}
 with open('input.txt', 'r') as fp:
-
-    for c,line in enumerate(fp):
+    for c, x in enumerate(fp):
         particle = []
-        line = line.rstrip().split(', ')
-
-        for p in line:
+        for p in x.rstrip().split(', '):
             p = p.split(',')
             particle.append(int(p[0][3:]))
             particle.append(int(p[1]))
             particle.append(int(p[2][:-1]))
         particles[c] = particle
 
+# Over time, the particle with the smallest combined acceleration will always be closest to 000
+pos = -1
+acc = 100000
+for c,v in particles.items():
+    cacc = abs(v[6]) + abs(v[7]) + abs(v[8])
 
-# For each particle, it provides the X, Y, and Z coordinates for the
-# particle's position (p), velocity (v), and acceleration (a), each in the
-# format <X,Y,Z>.
+    if cacc < acc:
+        acc = cacc
+        pos = c
+    elif pos == '-1':
+        acc = cacc
+        pos = c
 
-# Each tick, all particles are updated simultaneously. A particle's
-# properties are updated in the following order:
-
-
-for i in range(1000):
-
-    for particle in particles:
-        # [p(x,y,z), v(x,y,z), a(x,y,z)]
-        #    0,1,2     3,4,5     6,7,8
-
-        # Increase the X velocity by the X acceleration.
-        particle[3] = particle[3] + particle[6]
-        # Increase the Y velocity by the Y acceleration.
-        particle[4] = particle[4] + particle[7]
-        # Increase the Z velocity by the Z acceleration.
-        particle[5] = particle[5] + particle[8]
-        # Increase the X position by the X velocity.
-        particles[0] = particles[0] + particles[3]
-        # Increase the Y position by the Y velocity.
-        particles[1] = particles[1] + particles[4]
-        # Increase the Z position by the Z velocity.
-        particles[2] = particles[2] + particles[5]
-
-shortest = -1
-
-for n,particle in particles.items():
-    # Calculate
-
-    x = abs(0 - particle[0])
-    y = abs(0 - particle[1])
-    z = abs(0 - particle[2])
-
-    distance = x + y + z
-
-    if distance >
+print("particle: {}, acc: {}".format(pos, acc))
