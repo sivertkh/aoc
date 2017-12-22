91fccfae65a9a87e044830a0b31be644d562c34d
Sivert Hatteberg <sivert.hatteberg@usit.uio.no>
2017-12-22 15:44:23 +0100

    more work

--- 2017/day-21/day-21-1.py
+++ 2017/day-21/day-21-1.py
@@ -5,7 +5,7 @@ import numpy as np
 
 rules = {}
 
-with open('input-simple.txt', 'r') as fp:
+with open('input.txt', 'r') as fp:
     for x in fp:
         line = x.rstrip().split(' => ')
         k = "".join([x for x in line[0] if x in '.#'])
@@ -15,13 +15,16 @@ with open('input-simple.txt', 'r') as fp:
 # When searching for a rule to use, rotate and flip the pattern as necessary.
 pattern = np.array([['.', '#', '.'], ['.', '.', '#'], ['#', '#', '#']])
 
-for k in range(2):
+for k in range(4):
+    print("-*-*-*-")
 
     split_size = 0
     if len(pattern) % 2 == 0:
         # split into 2x2 blocs
+        print("2x2 blocks!")
         split_size = 2
     elif len(pattern) % 3 == 0:
+        print("3x3 blocks!")
         # split into 3x3 blocs
         split_size = 3
     else:
@@ -35,40 +38,50 @@ for k in range(2):
 
     print("split_size: {}".format(split_size))
     print("split: {}".format(split))
-    print("-*-*-*-")
     i_offset = 0
+
     for i in range(split):
         j_offset = 0
         for j in range(split):
             y = i * split
             x = j * split
-            print("****")
-            print(pattern[y:y+split_size,x:x+split_size])
-            print("****")
             sub_array = pattern[y:y+split_size,x:x+split_size]
+
+
+            # there are a total of 8 permutatations: id, rot90, rot180, rot270, flipX, flipX after rot90, flipX after rot180, flipX after rot270
 
             # Flatten
             flatt = "".join(sub_array.flatten())
+            rotate = "".join(np.flip(sub_array, 1).flatten())
             if flatt in rules:
                 print("In pattern!")
                 new = rules[flatt]
-                print(new)
+            elif rotate in rules:
+                print("In rotate!")
+                new = rules[rotate]
             else:
 
-                for a in range(1,4):
-                    rotate = "".join(np.rot90(sub_array,a).flatten())
+                for a in range(1, 4):
+                    rotate = "".join(np.rot90(sub_array, a).flatten())
                     if rotate in rules:
+                        print("Found in rotate nr {}".format(a))
                         new = rules[rotate]
                         break
+                    flip = "".join(np.flip(np.rot90(sub_array, a), 1).flatten())
 
-            new_array[i_offset:split_size+1,j_offset:split_size+1] = new
+                    if flip in rules:
+                        print("Fount in fliprot! {}".format(a))
+                        new = rules[flip]
+                        break
 
+            new_array[i_offset:i_offset+split_size+1, j_offset:j_offset+split_size+1] = new
             j_offset = j_offset + split_size + 1
 
-            print(new_array)
+
             #while True:
                 # flatten and lookup
                 # if no hit, then try rotate and flip
+        i_offset = i_offset + split_size + 1
 
     pattern = new_array
 
@@ -81,3 +94,8 @@ for k in range(2):
 
 
 print(pattern)
+print(pattern.flatten())
+print(len(pattern.flatten()))
+
+print(len([x for x in pattern.flatten() if x == '#']))
+print(len([x for x in pattern.flatten() if x == '.']))
