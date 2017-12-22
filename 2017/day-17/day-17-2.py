e9a60c94abf5df70c9a2de54252bf97a2c748aee
Sivert Hatteberg <sivertkh@ifi.uio.no>
2017-12-22 21:59:58 +0100

    day 17 ok

--- 2017/day-17/day-17-2.py
+++ 2017/day-17/day-17-2.py
@@ -1,34 +1,17 @@
 # --- Day 17: Spinlock ---
-# part 1 - ok
+# part 2 -
 
 from collections import deque
 
 buffer = deque()
 buffer.append(0)
 
-cur_pos = 0
 step = 316
 
-for i in range(1,10000):
-    # step step nr of places
-    # if it wrap -> move to
-    new_pos = cur_pos
+last = {}
+for i in range(1, 50000001):
+    buffer.rotate(-step)
+    buffer.append(i)
 
-    if i % 100000 == 0:
-        print(i)
+print(buffer[buffer.index(0)+1])
 
-    for a in range(step):
-        new_pos = new_pos + 1
-
-        if new_pos >= len(buffer):
-            new_pos = 0
-    # insert i after the current position.
-    buffer.insert(new_pos+1, i)
-    cur_pos = new_pos+1
-
-
-for c,v in enumerate(list(buffer)):
-    if v == 0:
-        print(buffer[c+1])
-        print(list(buffer)[c-10:c+10])
-        exit(0)
