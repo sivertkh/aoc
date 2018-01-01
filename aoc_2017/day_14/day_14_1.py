67722e8f616154e266d3fee60ef8b6ba68ebcfc3
Sivert Hatteberg <sivertkh@ifi.uio.no>
2018-01-01 13:15:00 +0100

    added the missing zeros.. left padding each hex result

--- aoc_2017/day_14/day_14_1.py
+++ aoc_2017/day_14/day_14_1.py
@@ -4,14 +4,17 @@
 from aoc_2017.day_10.day_10_2 import create_hash
 
 data = 'vbqugkhl'
+#data = 'flqrgnkx'
 
 grid = []
 for i in range(128):
-    grid.append([int(x) for x in bin(int(create_hash("{}-{}".format(data, i)),
-                                         16))[2:]])
+    tmp = [x for x in create_hash("{}-{}".format(data, i))]
+    tmp2 = [bin(int(x, 16))[2:].rjust(4, "0") for x in tmp]
+    grid.append([int(item) for sublist in tmp2 for item in sublist])
 
 s = 0
 for line in grid:
     s += sum(line)
 
 print(s)
+
