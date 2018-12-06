464db34f92ffcef59079788b2b06bfc42b0671de
Sivert Hatteberg <sivertkh@ifi.uio.no>
2018-12-06 07:29:28 +0100

    Start of day 6

--- aoc_2017/day_07/day_7_2.py
+++ aoc_2017/day_07/day_7_2.py
@@ -6,14 +6,12 @@ class Node:
 
     def __init__(self, name, weight=None, edges=None):
         self.name = name
-
         if weight is not None:
             self.weight = int(weight[1:-1])
         else:
             self.weight = None
 
         self.edges = edges
-
         self.combined_weight = None
 
     def __str__(self):
@@ -26,11 +24,10 @@ class Node:
         self.edges = edges
 
     def tree_weight(self):
-
         if self.edges is None:
             return self.weight
         else:
-            sub_tree_weigth = [e.tree_weight() for e in self.edges]
+            sub_tree_weigth = [edge.tree_weight() for edge in self.edges]
             sub_tree_weigth.append(self.weight)
 
             self.combined_weight = sum(sub_tree_weigth)
@@ -52,7 +49,9 @@ def create_tree():
 
     with open('input.txt', 'r') as fp:
         for line in fp:
+
             n = line.rstrip().split(" ")
+
             if len(n) > 2:
                 edge_ids = [x.rstrip(',') for x in n[3:]]
 
@@ -62,12 +61,6 @@ def create_tree():
                         nodes[edge_id] = Node(edge_id)
                     edges.append(nodes[edge_id])
 
-                if n[0] == 'httspr':
-                    print(n)
-                    if n[0] in nodes:
-                        print('JA!')
-                    else:
-                        print('NEI!')
                 if n[0] in nodes:
                     nodes[n[0]].set_weight(n[1])
                     nodes[n[0]].set_edges(edges)
@@ -75,7 +68,11 @@ def create_tree():
                     nodes[n[0]] = Node(n[0], n[1], edges)
             else:
                 # simple node
-                nodes[n[0]] = Node(n[0], n[1])
+                if n[0] not in nodes:
+
+                    nodes[n[0]] = Node(n[0], n[1])
+                else:
+                    nodes[n[0]].set_weight(n[1])
 
     # We do only need to look at nodes with edges.
     ed = [v for k, v in nodes.items() if v.edges is not None]
@@ -98,10 +95,10 @@ def create_tree():
 if __name__ == "__main__":
 
     nodes, root = create_tree()
-    print(root.tree_weight())
+    # print(root.tree_weight())
 
     # Quick and dirty.. look at the full tree and find the subtree with problems..
     # Calculate the diff manually..
-# print_tree(nodes, nodes[root], 0)
-    root = nodes['yruivis']
+    # print_tree(nodes, nodes[root], 0)
+    # root = nodes['yruivis']
     root.print_tree()
