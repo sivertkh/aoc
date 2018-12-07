ce6670262156ce6e44cfdc0074a9f21bc2bf83b8
Sivert Hatteberg <sivert.hatteberg@usit.uio.no>
2018-12-07 09:28:06 +0100

    Day 7 done

--- aoc_2018/day_07/day_7_2.py
+++ aoc_2018/day_07/day_7_2.py
@@ -1,6 +1,5 @@
 # --- Day 7: The Sum of Its Parts ---
-# Part 2 -
-
+# Part 2 - ok
 
 with open('input.txt') as fp:
     data = [x.split(' ') for x in fp.read().split('\n') if x]
@@ -9,57 +8,43 @@ requirments = {}
 for i in data:
     if i[7] not in requirments:
         requirments[i[7]] = set()
-
     requirments[i[7]].add(i[1])
 
-# Find starts
 all_req = []
 for k, v in requirments.items():
     all_req = all_req + list(v)
 
-
 nr_workers = 5
 step_base = 60
-
 possible = list(set(all_req) - set(requirments.keys()))
 path = []
-
 workers = []
 
 for i in range(nr_workers):
-    workers[i] = {'nr': i,
-                  'job': None,
-                  'work_time': 0,
-                  'comp_time': 0,
-                  'free': True,
-                  }
-
+    workers.append({'id': i,
+                    'job': '.',
+                    'work_time': 0,
+                    'comp_time': 0,
+                    'free': True
+                  })
 
 sec = 0
-total_time = 0
-while len(requirments) > 0:
-
-    # Update work time
+comp_times = {chr(x): x-64+step_base for x in range (65, 91)}
+while True:
     comp = []
 
     for x in [x for x in workers if not x['free']]:
         x['work_time'] += 1
-
         if x['work_time'] == x['comp_time']:
-            total_time += x['work_time']
             path.append(x['job'])
             comp.append(x['job'])
-            x['job'] = None
+            x['job'] = '.'
             x['work_time'] = 0
             x['comp_time'] = 0
             x['free'] = True
 
-    # Remove completed from requriments lists and
-    # recalculate
-
     new_req = {}
     for k, v in requirments.items():
-        print(v)
         for c in comp:
             if c in v:
                 v.remove(c)
@@ -70,22 +55,21 @@ while len(requirments) > 0:
             possible.append(k)
     requirments = new_req
 
-    # recalculate and assign to free workers
     possible.sort()
+    free_workers = [x for x in workers if x['free']]
+    for worker in free_workers:
+        if len(possible) > 0:
+            worker['free'] = False
+            worker['job'] = possible[0]
+            worker['comp_time'] = comp_times[possible[0]]
+            possible = possible[1:]
 
     free_workers = [x for x in workers if x['free']]
-
-    for worker in free_workers:
-
-        if len(possible) == 0:
-            break
-
-        worker['free'] = False
-        worker['job'] = possible[0]
-        worker['comp_time'] = 0
-
-        possible = possible[1:]
+    if len(free_workers) == nr_workers:
+        # Done when no workers are assigned
+        break
 
     sec += 1
 
 print(''.join(path))
+print(sec)
