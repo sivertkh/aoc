8603965e2af6d7c0d55bca0986d6f3e35c68c95a
Sivert Hatteberg <sivert.hatteberg@usit.uio.no>
2018-12-13 10:05:50 +0100

    Day 13 done

--- aoc_2018/day_12/day_12_2.py
+++ aoc_2018/day_12/day_12_2.py
@@ -1,6 +1,8 @@
 # --- Day 12: Subterranean Sustainability ---
 # Part 2 - ok
 
+import numpy as np
+
 with open('input.txt') as fp:
     tmp = fp.read().split('\n')
     init_state = list(tmp[0].split(' ')[-1])
@@ -9,15 +11,15 @@ with open('input.txt') as fp:
 # Padd the state (guesstimation..)
 left_pad_size = 20
 right_pad_size = 200
-left_pad = ['.' for x in range(left_pad_size)]
-right_pad = ['.' for x in range(right_pad_size)]
+left_pad = np.array(['.' for x in range(left_pad_size)])
+right_pad = np.array(['.' for x in range(right_pad_size)])
 state = left_pad + init_state + right_pad
 
 gen = 50000000000
 collaps_gen = 0
 
-for i in range(1, gen+1):
-    new_state = [x for x in state]
+for i in range(1, 5000):
+    new_state = np.array([x for x in state])
     for j in range(2, len(state)-1):
         current = ''.join(state[j-2:j+3])
 
@@ -36,9 +38,6 @@ for i in range(1, gen+1):
 
     state = new_state
 
-    if state[-3] == '#' or state[2] == '#':
-        print('Overflow!!')
-        exit()
 
-nr = [x+(gen-collaps_gen) for x in range(-left_pad_size, len(state))]
-print(sum([nr[k] for k, x in enumerate(state) if x == '#']))
+nr = np.array[x+(gen-collaps_gen) for x in range(-left_pad_size, len(state))])
+print(np.array([nr[k] for k, x in enumerate(state) if x == '#']).sum())
