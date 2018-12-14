d0adc137d76e5831419e1df9931a7c1bd76f58b8
Sivert Hatteberg <sivert.hatteberg@usit.uio.no>
2018-12-14 09:46:43 +0100

    Day 14 done

--- aoc_2018/day_14/day_14_2.py
+++ aoc_2018/day_14/day_14_2.py
@@ -1,39 +1,27 @@
 # --- Day 14: Chocolate Charts ---
-# Part 2 -
+# Part 2 - ok
 
-from collections import deque
-import itertools
-
-recipes = deque([3, 7])
+recipes = [3, 7]
 
 elf_a = 0
 elf_b = 1
 
 nr = [8, 8, 0, 7, 5, 1]
 
-count = 0
-#nr = [5, 1, 5, 8, 9]
 while True:
 
-    if count % 10000 == 0:
-        print(count)
-    count += 1
     new = recipes[elf_a] + recipes[elf_b]
-
-    # Split into digets
     recipes.extend([int(x) for x in str(new)])
 
     elf_a += recipes[elf_a] + 1
+    elf_a %= len(recipes)
     elf_b += recipes[elf_b] + 1
+    elf_b %= len(recipes)
 
-    while elf_a >= len(recipes):
-        elf_a -= len(recipes)
-
-    while elf_b >= len(recipes):
-        elf_b -= len(recipes)
-
-    end = list(recipes)[-len(nr):]
-
-    if end == nr:
+    if recipes[-len(nr):] == nr: 
         print(len(recipes)-len(nr))
+        exit(0)
+        
+    elif recipes[-len(nr)-1:-1] == nr:
+        print(len(recipes)-len(nr)-1)
         exit(0)
