47351f6d68db834cef72bdc000087cae83852321
Sivert Hatteberg <sivert.hatteberg@usit.uio.no>
2018-12-20 22:17:44 +0100

    day 20 part 1 done

--- aoc_2018/day_20/day_20_1.py
+++ aoc_2018/day_20/day_20_1.py
@@ -1,97 +1,49 @@
 # --- Day 20: A Regular Map ---
-# Part 1 -
+# Part 1 - ok
 
 from collections import deque
 
+movments = ['E', 'S', 'W', 'N']
 
-class Mapexer(object):
+def parse(stack):
+    results = []
+    tmp = 0
+    while len(stack) > 0:
+        c = stack.popleft()
+        if c == '^':
+            continue
+        elif c == '$':
+            results.append(tmp)
+            return max(results)
+        if c == '(':
+            tmp += parse(stack)
+        elif c == ')':
 
-    def __init__(self):
-        #self.mapex = deque(list('^ENWWW(NEEE|SSE(EE|N))$'))
-        self.mapex = deque(list('^WNE$'))
-        print(self.mapex)
-        self.movments = ['N', 'S', 'E', 'W']
+            if tmp == 0:
+                # This could give the wrong result if there is a 
+                # large loop.
+                return 0
 
-        self.size = len([x for x in self.mapex if x in self.movments])
-        print(self.size)
-        self.size = 10
-        self.area = [[' ' for x in range(self.size)] for y in range(self.size)]
-
-        # Start position
-        self.pos_x = self.size // 2
-        self.pos_y = self.size // 2
-
-    def add_walls(self):
-
-        for x in range(self.size):
-            for y in range(self.size):
-                if self.area[x][y] == ' ' or self.area[x][y] == '?':
-                    self.area[x][y] = '#'
-
-    def add_unknown(self, x, y):
-        neighbours = [
-            [x+1, y],
-            [x-1, y],
-            [x, y+1],
-            [x, y-1]
-        ]
-        # Fill inn unknown
-        for n in neighbours:
-            if self.area[n[0]][n[1]] == ' ':
-                self.area[n[0]][n[1]] = '?'
-
-    def move(self, direction):
-        assert direction in self.movments
-
-        start_x = self.pos_x
-        start_y = self.pos_y
-
-        if direction == 'N':
-            self.area[self.pos_x-1][self.pos_y] = '-'
-            self.add_unknown(self.pos_x-1, self.pos_y)
-            self.pos_x -= 2
-        elif direction == 'S':
-            self.area[self.pos_x+1][self.pos_y] = '-'
-            self.add_unknown(self.pos_x+1, self.pos_y)
-            self.pos_x += 2
-        elif direction == 'E':
-            self.area[self.pos_x][self.pos_y+1] = '|'
-            self.add_unknown(self.pos_x, self.pos_y+1)
-            self.pos_y += 2
+            results.append(tmp)
+            return max(results)
+        elif c == '|':
+            results.append(tmp)
+            tmp = 0
+        elif c in movments:
+            tmp += 1
         else:
-            self.area[self.pos_x][self.pos_y-1] = '|'
-            self.add_unknown(self.pos_x, self.pos_y-1)
-            self.pos_y -= 2
+            print(f'??? {c}')
 
-        self.area[start_x][start_y] = '.'
-        self.area[self.pos_x][self.pos_y] = 'X'
 
-        self.add_unknown(self.pos_x, self.pos_y)
+def main():
 
-    def print_map(self):
-        while len(self.mapex) > 0:
+    with open('input.txt') as fp:
+        mapex = deque(list(fp.read().rstrip('\n')))
 
-            c = self.mapex.popleft()
-            if c == '^':
-                continue
-            elif c == '$':
-                print('At end!!')
-                assert len(self.mapex) == 0
-            elif c == '(':
-                while True:
-                    # Pop from the stack until we have a valid subregex
-                    c = self.mapex.pop()
-            else:
-                print(c)
-                self.move(c)
-                # There should only be letters here
-                assert c in self.movments
-
-        self.add_walls()
-        for i in self.area:
-            print(''.join(i))
-
+    print(parse(mapex))
+    
 
 if __name__ == "__main__":
-    a = Mapexer()
-    a.print_map()
+
+    main()
+
