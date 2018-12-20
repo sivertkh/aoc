72fe5e61217d654229aca0c3028990f296abffe2
Sivert Hatteberg <sivertkh@ifi.uio.no>
2018-12-20 23:38:07 +0100

    Day 20 done

--- aoc_2018/day_20/day_20_2.py
+++ aoc_2018/day_20/day_20_2.py
@@ -1,92 +1,45 @@
 # --- Day 20: A Regular Map ---
-# Part 2
+# Part 2 - ok
 
 from collections import deque
-
-movments = ['E', 'S', 'W', 'N']
-room_count = 0
-
-class Room(object):
-
-    def __init__(self, value):
-        self.value = value
-        self.rooms = []
-
-    def add_room(self, room):
-
-        if room is not None:
-            self.rooms.append(room)
-
-    def get_rooms(self):
-        return self.rooms
-
-
-def parse(subtree, subtree_distance):
-
-
-    tmp = 0
-
-    subtree_root = None
-    current_room = None
-
-    while len(subtree) > 0:
-        c = subtree.popleft()
-
-        if c == '^':
-            continue
-        elif c == '$':
-            return 
-        if c == '(':
-            current_room.add_room(parse(subtree, subtree_distance+tmp))
-
-        elif c == ')':
-            return subtree_root 
-        elif c == '|':
-            tmp = 0
-
-
-        elif c in movments:
-            print(subtree_distance + tmp)
-            if not subtree_root:
-                current_room = Room(subtree_distance+tmp)
-                subtree_root = current_room
-            else:
-                tmp += 1
-                tmp_room = Room(subtree_distance+tmp)
-                current_room.add_room(tmp_room)
-                current_room = tmp_room
-
-        else:
-            print(f'??? {c}')
-
-
-def find_lenght(root, current_pos):
-    global room_count
-
-    if current_pos > 1000:
-        room_count += 1
-
-    if len(root.get_rooms()) == 0:
-        return
-
-    for i in root.get_rooms:
-        find_lenght(i, current_pos+1)
-
+import networkx
 
 
 def main():
 
     with open('input.txt') as fp:
-        mapex = deque(list(fp.read().rstrip('\n')))
+        mapex = list(fp.read().rstrip('\n'))[1:-1]
 
-    
-    root = parse(mapex, 0)
-    find_lenght(root, 0)
+    graf = networkx.Graph()
+    pos_x = 0
+    pos_y = 0
+    stack = deque()
 
-    print(room_count)
+    for c in mapex:
+        if c == '(':
+            stack.append((pos_x, pos_y))
+        elif c == '|':
+            pos_x, pos_y = stack[-1]
+        elif c == ')':
+            pos_x, pos_y = stack.pop()
+        else:
+            from_x = pos_x
+            from_y = pos_y
+            if c == 'E':
+                pos_x += 1
+            elif c == 'S':
+                pos_y += 1
+            elif c == 'W':
+                pos_x -= 1
+            else:
+                pos_y -= 1
+
+            graf.add_edge((from_x, from_y), (pos_x, pos_y))
+
+    lengths = networkx.algorithms.shortest_path_length(graf, (0, 0))
+    print(sum([1 for x in lengths.values() if x >= 1000]))
 
 
 if __name__ == "__main__":
 
     main()
-
