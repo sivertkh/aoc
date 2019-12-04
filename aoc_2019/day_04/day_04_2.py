e76bace7ac6f8b3bb7807a96666188396a2a1369
Sivert Hatteberg <sivert.hatteberg@usit.uio.no>
2019-12-04 19:38:12 +0100

    part 1 done and start of part 2

---
+++ aoc_2019/day_04/day_04_2.py
@@ -0,0 +1,24 @@
+# --- Day 4: Secure Container ---
+# Part 2 -
+
+with open('./input.txt') as fp:
+    pw_range = [int(x) for x in fp.read().split('-') if x]
+
+matches = 0
+
+for nr in range(pw_range[0], pw_range[1]):
+    nr_split = [int(i) for i in str(nr)]
+    # Two adjacent digits
+    if min([len(set(x)) for x in zip(nr_split[:-1], nr_split[1:])]) != 1:
+        continue
+    # Never decreasing
+    nr_sorted = nr_split[:]
+    nr_sorted.sort()
+    if nr_split != nr_sorted:
+        continue
+
+
+
+    matches += 1
+
+print(matches)
