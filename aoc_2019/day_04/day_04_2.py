5c5b5c7f04b5b0af90e3f1a74d722c00c0cc458a
Sivert Hatteberg <sivertkh@ifi.uio.no>
2019-12-05 23:06:16 +0100

    Day 4 done

--- aoc_2019/day_04/day_04_2.py
+++ aoc_2019/day_04/day_04_2.py
@@ -1,10 +1,12 @@
 # --- Day 4: Secure Container ---
-# Part 2 -
+# Part 2 - ok
 
 with open('./input.txt') as fp:
     pw_range = [int(x) for x in fp.read().split('-') if x]
 
 matches = 0
+
+#pw_range = [112233, 112234]
 
 for nr in range(pw_range[0], pw_range[1]):
     nr_split = [int(i) for i in str(nr)]
@@ -17,7 +19,24 @@ for nr in range(pw_range[0], pw_range[1]):
     if nr_split != nr_sorted:
         continue
 
+    duplates = []
+    current_series = None
+    last_nr = []
+    for n in nr_split:
+        if len(last_nr) == 0:
+            last_nr.append(n)
 
+        elif n == last_nr[0]:
+            last_nr.append(n)
+        else:
+            duplates.append(last_nr)
+            last_nr = [n]
+
+    if len(last_nr) >= 2:
+        duplates.append(last_nr)
+
+    if not 2 in [len(x) for x in duplates]:
+        continue
 
     matches += 1
 
