0cde28213376cfb259e15d49bcebd378f2d9ed18
Sivert Hatteberg <sivertkh@ifi.uio.no>
2020-12-01 09:05:08 +0100

    2020 day 1 done

---
+++ aoc_2020/day_01/day_01.py
@@ -0,0 +1,21 @@
+import itertools
+
+# --- Day 1: Report Repair ---
+
+with open('./input.txt') as fp:
+    data = set([int(x.rstrip()) for x in fp.readlines()])
+
+find = set([2020-x for x in data])
+result = list(set(data).intersection(set(find)))
+print(f"Part 1: {result[0]*result[1]}")
+
+# --- Part Two ---
+
+for x in data:
+    part_sum = 2020-x
+    possible_values = list(set(range(part_sum+1)).intersection(data))
+    result = [y for y in list(itertools.combinations(possible_values, 2))
+              if y[0] + y[1] == part_sum]
+    if result:
+        print(f'Part 2: {x * result[0][0] * result[0][1]}')
+        break
