7bf59269a42d4d14cdbad7681b17c4bc8e1d9216
Sivert Hatteberg <sivertkh@ifi.uio.no>
2020-12-04 14:24:51 +0100

    A bit simpler.. :)

--- 2020/03/day_03.py
+++ 2020/03/day_03.py
@@ -5,7 +5,9 @@ from functools import reduce
 with open('./input.txt') as fp:
     m = [list(x.rstrip()) for x in fp.readlines()]
 
-res = [sum([1 for x in y if m[x[0]][x[1] % len(m[0])] == "#"]) for y in [list(zip(range(x[1], len(m), x[1]), range(x[0], (len(m)//x[1])*x[0]+1, x[0]))) for x in [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]]]]
+slopes = [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]]
+positions = [list(zip(range(x[1], len(m), x[1]), range(x[0], (len(m)//x[1])*x[0]+1, x[0]))) for x in slopes]
+hits = [sum([m[y[0]][y[1] % len(m[0])] == "#" for y in x]) for x in positions]
 
-print(f'Part 1: {res[1]}')
-print(f'Part 2: {reduce(lambda x ,y: x*y, res)}')
+print(f'Part 1: {hits[1]}')
+print(f'Part 2: {reduce(lambda x ,y: x*y, hits)}')
