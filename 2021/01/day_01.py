e853c5bfa4001672faeb7d14416a6bfffee3b8a7
Sivert Kronen Hatteberg <skh@uio.no>
2021-12-01 06:45:24 +0100

    Day 1 2021 done

---
+++ 2021/01/day_01.py
@@ -0,0 +1,13 @@
+# --- Day 1: Sonar Sweep ---
+
+with open("input.txt") as fp:
+    depths = [int(x.strip()) for x in fp.readlines()]
+
+part_1 = sum(list(map(lambda a, b: 1 if b > a else 0, depths[:-1], depths[1:])))
+depths = list(zip(depths[:-2], depths[1:-1], depths[2:]))
+part_2 = sum(
+    list(map(lambda a, b: 1 if sum(b) > sum(a) else 0, depths[:-1], depths[1:]))
+)
+
+print(f"Part 1: {part_1}")
+print(f"Part 2: {part_2}")
