f1cf6190e6139cfed62bd62683d1640b9aa415eb
Sivert Kronen Hatteberg <skh@uio.no>
2021-12-02 14:24:04 +0100

    Improvements

--- 2021/02/day_02.py
+++ 2021/02/day_02.py
@@ -4,23 +4,19 @@ with open("input.txt") as fp:
     moves = [x.strip().split() for x in fp.readlines()]
 
 horizontal = 0
-depth_part_1 = 0
-
 aim = 0
-depth_part_2 = 0
+depth = 0
 
-for move in moves:
-
-    match move[0]:
+for direction, stept in moves:
+    step = int(stept)
+    match direction:
         case "down":
-            depth_part_1 += int(move[1])
-            aim += int(move[1])
+            aim += step
         case "up":
-            depth_part_1 -= int(move[1])
-            aim -= int(move[1])
+            aim -= step
         case "forward":
-            horizontal += int(move[1])
-            depth_part_2 += aim * int(move[1])
+            horizontal += step
+            depth += aim * step
 
-print(f"Part 1: {horizontal * depth_part_1}")
-print(f"Part 2: {horizontal * depth_part_2}")
+print(f"Part 1: {horizontal * aim}")
+print(f"Part 2: {horizontal * depth}")
