99ac765e1c2e03adf1bdd913d75a7b8e6a4fbe00
Sivert Kronen Hatteberg <skh@uio.no>
2021-12-12 08:02:33 +0100

    Simplify into one common solver function

--- 2021/12/day_12.py
+++ 2021/12/day_12.py
@@ -3,21 +3,9 @@
 import networkx as nx
 
 
-def generate_part_1_paths(graph, cur_path):
-    res = []
-    for a in graph.adj[cur_path[-1]]:
-        if a.islower() and a in cur_path:
-            continue
-        p = cur_path + [a]
-        if a == 'end':
-            res.append(p)
-            continue
-        res.extend(generate_part_1_paths(graph, p))
-    return res
-
-
-def generate_part_2_paths(graph, cur_path, used_small=False):
-    res = []
+def generate_paths(graph, cur_path, used_small=False):
+    res_1 = []
+    res_2 = []
     for a in graph.adj[cur_path[-1]]:
         if a == 'start':
             continue
@@ -28,14 +16,19 @@ def generate_part_2_paths(graph, cur_path, used_small=False):
             local_used_small = True
         p = cur_path + [a]
         if a == 'end':
-            res.append(p)
+            if not used_small:
+                res_1.append(p)
+            res_2.append(p)
             continue
-        res.extend(generate_part_2_paths(graph, p, used_small=local_used_small))
-    return res
+        r1, r2 = generate_paths(graph, p, used_small=local_used_small)
+        res_1 += r1
+        res_2 += r2
+    return res_1, res_2
 
 
 with open("input.txt") as fp:
     G = nx.Graph([[y for y in x.strip().split('-')] for x in fp.readlines()])
 
-print(f"Part 1: {len(generate_part_1_paths(G, ['start']))}")
-print(f"Part 2: {len(generate_part_2_paths(G, ['start']))}")
+r1, r2 = generate_paths(G, ['start'])
+print(f"Part 1: {len(r1)}")
+print(f"Part 2: {len(r2)}")
