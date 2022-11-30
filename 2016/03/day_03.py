4280c8a10c9dad06d697dd5f3f34bd40f5f82851
Sivert Kronen Hatteberg <sivert.hatteberg@usit.uio.no>
2022-12-01 00:02:47 +0100

    improvements to old code

--- 2016/03/day_03.py
+++ 2016/03/day_03.py
@@ -1,17 +1,31 @@
 # --- Day 3: Squares With Three Sides ---
 
-import itertools'[]
+import itertools
 import numpy as np
 
 
-def possible_triangles(triangles):
-    return len(triangles) - len([y for y in [[x for x in itertools.permutations(t, 3) if sum(x[:-1]) <= x[-1]] for t in triangles] if y])
+def load_input() -> np.array:
+    with open("./input.txt") as fp:
+        triangles = np.array(
+            [list(map(int, x.rstrip().split())) for x in fp.readlines()]
+        )
+    return triangles
 
 
-with open('./input.txt') as fp:
-    triangles = np.array([list(map(int, x.rstrip().split())) for x in fp.readlines()])
+def possible_triangles(triangles: np.array) -> int:
+    return len(triangles) - len(
+        [
+            y
+            for y in [
+                [x for x in itertools.permutations(t, 3) if sum(x[:-1]) <= x[-1]]
+                for t in triangles
+            ]
+            if y
+        ]
+    )
 
-print(f'Part 1: {possible_triangles(triangles)}')
 
+triangles = load_input()
+print(f"Part 1: {possible_triangles(triangles)}")
 triangles = np.reshape(np.transpose(triangles), triangles.shape)
-print(f'Part 2: {possible_triangles(triangles)}')
+print(f"Part 2: {possible_triangles(triangles)}")
